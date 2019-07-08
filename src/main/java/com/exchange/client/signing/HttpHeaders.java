f10dbe4e92a63780974405750991d62fdc442972
AllSecure <41504556+allsecure-pay@users.noreply.github.com>
2019-07-08 10:33:39 +0200

    Add files via upload

---
+++ src/main/java/com/exchange/client/signing/HttpHeaders.java
@@ -0,0 +1,8 @@
+package com.exchange.client.signing;
+
+public final class HttpHeaders {
+	private HttpHeaders() {}
+
+	public static final String AUTHORIZATION = "Authorization";
+	public static final String DATE = "Date";
+}
